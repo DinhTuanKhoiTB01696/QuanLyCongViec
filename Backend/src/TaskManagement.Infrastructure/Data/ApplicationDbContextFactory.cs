baab1d257c7d728600935267c3d649a3764de49d
DinhTuanKhoiTB01696 <khoidttb01696@Gmail.com>
2026-07-23 17:43:06 +0700

    Fix CI unit test database filtering

--- Backend/src/TaskManagement.Infrastructure/Data/ApplicationDbContextFactory.cs
+++ Backend/src/TaskManagement.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -11,8 +11,6 @@ namespace TaskManagement.Infrastructure.Data
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            optionsBuilder.UseSqlServer("Server=KHOI\\SQLEXPRESS;Database=TaskManagementDB;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
-
             optionsBuilder.UseSqlServer("Server=KHOI\\SQLEXPRESS;Database=TaskManagementDB;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;Encrypt=False;Connection Timeout=60;", opts => opts.CommandTimeout(180));
 
             return new ApplicationDbContext(optionsBuilder.Options, null, null);
