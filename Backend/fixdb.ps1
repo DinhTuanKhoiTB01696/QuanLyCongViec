fe7fe2317889ee29d354fd22d99f910527344089
DinhTuanKhoiTB01696 <khoidttb01696@Gmail.com>
2026-07-23 17:34:19 +0700

    Remove unrelated generated and temporary files

--- Backend/fixdb.ps1
+++
@@ -1,8 +0,0 @@
-$conn = New-Object System.Data.SqlClient.SqlConnection
-$conn.ConnectionString = 'Server=KHOI\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True'
-$conn.Open()
-$cmd = $conn.CreateCommand()
-$cmd.CommandText = 'ALTER DATABASE TaskManagementDB SET MULTI_USER WITH ROLLBACK IMMEDIATE'
-$cmd.ExecuteNonQuery()
-$conn.Close()
-Write-Host 'Done'
